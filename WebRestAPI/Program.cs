5f51948a1d5553b82ce71ba7d0f6cfe291b00938
Joel Njuguna <joel@wifi-roaming-128-4-146-187.wifi.udel.edu>
2025-12-02 18:14:23 -0500

    More Changes

--- WebRestAPI/Program.cs
+++ WebRestAPI/Program.cs
@@ -2,15 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options. JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UserSwaggerUI();
     app.MapOpenApi();
 }
 
